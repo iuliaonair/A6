bc7cec797c23c573fe6a27238cc9638713081ee7
iuliaonair <99997735+iuliaonair@users.noreply.github.com>
2023-05-11 21:35:13 -0700

    final result

--- encrypt-it.js
+++ encrypt-it.js
@@ -1,30 +1,77 @@
 (function() {
     "use strict";
-  
-    /**
-     * The starting point in our program, setting up a listener
-     * for the "load" event on the window, signalling the HTML DOM has been constructed
-     * on the page. When this event occurs, the attached function (init) will be called.
-     */
+
     window.addEventListener("load", init);
   
-    /**
-     * TODO: Write a function comment using JSDoc.
-     */
     function init() {
-      // Note: In this function, we usually want to set up our event handlers
-      // for UI elements on the page.
-      var encryptButton = document.getElementById("encryptButton");
+      // Encrypt button functionality 
+      var encryptButton = document.getElementById("encrypt-it");
       encryptButton.addEventListener("click", handleClick);
+
+      // Reset button functionality 
+      var resetButton = document.getElementById("reset");
+      resetButton.addEventListener("click", reset);
     }
   
-    // Add any other functions in this area (you should not implement your
-    // entire program in the init function, for similar reasons that
-    // you shouldn't write an entire Java program in the main method).
+    /**
+    * Handles the click event of the "Encrypt It!" button.
+    */
     function handleClick() {
-        console.log("Button clicked!");
-        // Add any other code you want to execute when the button is clicked
+        var inputTextArea = document.getElementById("input-text");
+        var outputParagraph = document.getElementById("result");
+        var cipherType = document.getElementById("cipher-type").value;
+        var fontSize = document.querySelector('input[name="text-size"]:checked').value;
+        var allCaps = document.getElementById("all-caps").checked;
+    
+        var text = inputTextArea.value;
+    
+        // Apply cipher
+        var encryptedText = shiftCipher(text);
+    
+        // Apply uppercase
+        if (allCaps) {
+          encryptedText = encryptedText.toUpperCase();
+        }
+        // Apply font size
+        outputParagraph.style.fontSize = fontSize;
+    
+        // Set the encrypted text as the result
+        outputParagraph.textContent = encryptedText;
     }
+
+    /**
+   * Resets the input textarea and the result paragraph to their initial state.
+   */
+    function reset() {
+        var inputTextArea = document.getElementById("input-text");
+        var outputParagraph = document.getElementById("result");
+    
+        inputTextArea.value = "";
+        outputParagraph.textContent = "";
+    }
+
+
+    /**
+    * Returns an encrypted version of the given text, where
+    * each letter is shifted alphabetically ahead by 1 letter,
+    * and 'z' is shifted to 'a' (creating an alphabetical cycle).
+    */
+    function shiftCipher(text) {
+        text = text.toLowerCase();
+        let result = "";
+        for (let i = 0; i < text.length; i++) {
+          if (text[i] < 'a' || text[i] > 'z') {
+            result += text[i];
+          } else if (text[i] == 'z') {
+            result += 'a';
+          } else {
+            let letter = text.charCodeAt(i);
+            let resultLetter = String.fromCharCode(letter + 1);
+            result += resultLetter;
+          }
+        }
+        return result;
+    }
+
   
   })();
-  
